25345753ade522a11d5d4a472fd32665fc55a026
bbanderson84 <bbanderson84@gmail.com>
2019-01-22 19:28:31 -0700

    final copy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,11 @@ var count = 0;
 var userWins = 0;
 var userLosses = 0;
 
+$(document).ready (function () {
+
 
 /// function starts new game
 var newGame = function () {
-
-
 
     $(".crystals").empty();
 
@@ -52,6 +52,7 @@ var newGame = function () {
 
 newGame();
 
+// clicking on each crystal adds that specific crystals value to the user score
 $(document).on('click', ".crystal", function () {
 
     console.log($(this).attr("crystalvalue"));
@@ -66,10 +67,11 @@ $(document).on('click', ".crystal", function () {
 
     $("#user-score").html(count);
 
+    // if else is not working, it stopped working when i tried to add the newGame function for new game and reset. it does not display losses or wins , does not alert. This worked before I added newGame function
     if(count > randomNum){
 
         userLosses++;
-        console.log(userLosses);
+     
         alert("YOU LOST!");
 
         count = 0;
@@ -85,7 +87,7 @@ $(document).on('click', ".crystal", function () {
         alert("YOU WON!");
 
         userWins++; 
-        console.log(userWins);
+  
         count = 0;
 
         $("#user-wins").html(userWins);
@@ -97,210 +99,5 @@ $(document).on('click', ".crystal", function () {
     
 });
 
-//     var crystalValue = ($(this).attr("valueofcrystal"));
+});
 
-//     crystalValue = parseInt(crystalValue);
-
-//     count += crystalValue;
-
-//     console.log(count);
-
-//     $("#user-score").html(count);
-
-//     if (count > randomNum) {
-
-//         alert("YOU LOST!");
-
-//     } else if (count === randomNum) {
-
-//         alert("YOU WON!");
-//     }
-
-// });
-
-// });
-
-
-//     for (var i = 0; i < 4; i++) {
-
-//         var crystalPoints = Math.floor((Math.random() * 12) + 1);
-
-//         console.log(crystalPoints);
-
-//         var crystal = $("#crystalPic");
-
-//             crystal.attr({"id": "crystalPic", "data-valuecrystal": crystalPoints});
-
-//             $("#crystals").append(crystal);
-//     }
-
-// // for loop assigning random number from 1-12 to the diamond picture
-//     for (var i = 0; i < 1; i++) {
-
-//         var diamondPoints = Math.floor((Math.random() * 12) + 1);
-
-//         console.log(diamondPoints);
-
-//         var diamond = $("#diamondPic");
-
-//              diamond.attr({"id": "diamondPic", "data-valuediamond": diamondPoints});
-
-//             $("#diamonds").append(diamond);
-
-//     }
-
-// // for loop assigning random number from 1-12 to the ruby picture
-//     for (var i = 0; i < 1; i++){
-
-//         var rubyPoints = Math.floor((Math.random() * 12) + 1);
-
-//         console.log(rubyPoints);
-
-//         var ruby = $("#rubyPic");
-
-//             ruby.attr({"id": "rubyPic", "data-valueruby": rubyPoints});
-
-//             $("#rubys").append(ruby);
-
-
-//     }
-
-// // for loop assigning random number from 1-12 to the gem picture
-//     for (var i = 0; i < 1; i++){
-
-//         var gemPoints = Math.floor((Math.random() * 12) + 1);
-
-//         console.log(gemPoints);
-
-//         var gem = $("#gemPic");
-
-//             gem.attr({"id": "gemPic", "data-valuegem": gemPoints});
-
-//             $("#gems").append(gem);
-
-//     }
-
-// // }
-
-// // gameStart ();
-
-// // logs when user clicks crystal pic
-//     $("#crystalPic").on("click", function () {
-
-//         var crystalValue = ($(this).attr("data-valuecrystal"));
-
-//         crystalValue = parseInt(crystalValue);
-
-//         count += crystalValue;
-
-//         console.log(count);
-
-//         $("#user-score").html(count);
-
-
-//     // console.log($(this).attr("data-valuecrystal"));
-    
-// });
-
-// // logs when user clicks diamond pic
-// $("#diamondPic").on("click", function () {
-
-//     var diamondValue = ($(this).attr("data-valuediamond"));
-
-//     diamondValue = parseInt(diamondValue);
-
-//     count += diamondValue;
-
-//     console.log(count);
-
-//     $("#user-score").html(count);
-    
-// });
-
-// // logs when user clicks ruby pic
-// $("#rubyPic").on("click", function () {
-
-
-//     var rubyValue = ($(this).attr("data-valueruby"));
-
-//     rubyValue = parseInt(rubyValue);
-
-//     count += rubyValue;
-
-//     console.log(count);
-
-//     $("#user-score").html(count);
-
-    
-// });
-
-// // logs when user clicks gem pic
-// $("#gemPic").on("click", function () {
-
-//     // console.log($(this).attr("data-valuegem"));
-
-//     var gemValue = ($(this).attr("data-valuegem"));
-
-//     gemValue = parseInt(gemValue);
-
-//     count += gemValue;   
-
-//     console.log(count);
-
-//     $("#user-score").html(count);
-
-
-//     if (count > randomNum) {
-
-//         alert("YOU LOST!");
-
-//         userLosses++;
-
-//         console.log(userLosses);
-
-//         $("#user-losses").html(userLosses);
-
-
-//     }  else if (count === randomNum) {
-
-//         alert("YOU WON!");
-
-//         userWins++;
-
-//         console.log(userWins);
-
-//         $("user-wins").html(userWins);
-
-    
-//             // $("#user-losses").html(userLosses);
-    
-//         }
-
-    
-// });
-
-
-// $(document).ready( function () {
-
-    //     if (count > randomNum) {
-
-    //         alert("YOU LOST!");
-
-    //         userWins++;
-
-    //         console.log(userWins);
-
-    //         // $("#user-wins").html(userWins);
-
-
-    // }  else if (count === randomNum) {
-
-    // // alert("Sorry, You Lose. Try again!");
-
-
-    //      userLosses++;
-
-    //     // $("#user-losses").html(userLosses);
-
-    // }
-// });
